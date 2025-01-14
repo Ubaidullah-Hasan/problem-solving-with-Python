cfb58e459fb01fa1ac16f887d70d27941fb41a6b
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-01-14 21:44:39 +0600

    function

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
 # if else 
 if 5<10 :
     print("condition is true")
+    
+# recursion
+def factorial_result(num):
+    if(num>0):
+        print(num)
+        result = num + factorial_result(num - 1)
+    else:
+        result = 0
+    return result
+
+print("Recursion Example Results:")
+print(factorial_result(4))
